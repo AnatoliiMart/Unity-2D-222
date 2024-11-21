28db774690b44972a819235576201371cd83440a
Anatolii <anatolii.mart6@gmail.com>
2024-11-21 19:36:13 +0200

    >>>

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pipePrefab;
+    private float pipeSpawnPeriod = 4.0f; // 4 seconds
+    private float pipeTimeout;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
